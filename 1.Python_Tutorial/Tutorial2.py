d9e43af3dcfd06549e75ebc38f7fd553de574ff7
Alapan Banerjee <ronnieworkspace@gmail.com>
2023-06-08 13:27:38 +0530

    added Python_tutorial folder which includes Basic Python Programming for Beginners

---
+++ 1.Python_Tutorial/Tutorial2.py
@@ -0,0 +1,66 @@
+# Explaination of print function:
+# SYNTAX of a print function: 
+#print(argument)
+print('Hello, What is your Name?')
+
+# Taking Input from the user : 
+name = input()
+print("Hello,",name)
+
+
+############################
+
+
+# Basic Operators
+# '+' is the addition operator 
+# '-' is the substraction operator 
+# '*' is multiplication operator
+# '/' is division operator
+# '=' is the declaration operator
+print(64/10)
+
+# using Operators with Data Types
+num1 = 45
+num2 = 3
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
+##############################
+
+
+# Some More Operators 
+# '**' is the exponent operator
+# '//' is the integer division which gives us the integer part of the division
+print(64//10)
+# '%' is the modulus Opertor which gives us the remainder
+print (5%2)
+
+
+#using Operators with Data Types
+num3 = (num1**num2)
+print(num3)
+num3 = (num1//num2)
+print(num3)
+num3 = (num1%num2)
+print(num3)
+
+
+##############################
+
+# Conversion of numbers (Implicit / Explicit)
+
+
+print('Pick a number: ')
+num1 = input()
+print('Pick another number: ')
+num2 = input()
+print(type(num2))
+
+SUM = int(num1) + int(num2)
+print(SUM)
+
+
---
+++ 1.Python_Tutorial/Tutorial3.py
@@ -0,0 +1,26 @@
+# Comparison Operators
+
+# Always returns either 'TRUE' / 'FALSE'
+# '<' is the less than operator 
+# '>' is the greater than operator
+# '<=' is the less than equal to operator
+# '>=' is the greater than equal to operator 
+# '==' is equal to operator
+# '=' is the declaration operator (not a comparison operator)
+# '!=' is not equal to operator 
+
+
+# Conditions/Comparisons
+print(2<3)
+print(4>5)
+print('hello' == 'helo')
+print('hello' != 'helo')
+print('liz' == 'Liz')
+
+
+# Advanced Conditions
+print(2<3)
+print(2-3+4 > 5)
+print(False == True)
+
+
